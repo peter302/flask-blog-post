3e6e001d8e2f4d73af72054bc7e30b905115e0fe
peter kuria <you@example.com>
2020-01-18 05:47:27 -0500

    added update profile form

--- flask-blog/app/main/views.py
+++ flask-blog/app/main/views.py
@@ -13,6 +13,16 @@ def index():
 
     return render_template('index.html',title=title,posts=posts)
 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)    
+
 @main.route('/post',methods=['GET','POST'])
 @login_required
 def post():
